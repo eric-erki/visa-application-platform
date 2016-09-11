3d09806b40ea0962b5b5e59753785aaf5cf69bfd
Adagio-cantabile <375608371@qq.com>
2016-09-11 14:29:43 +0800

    add send message functionality

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,20 @@
+class MessagesController < ApplicationController
+  def new
+    @applicant = Applicant.find(params[:applicant])
+  end
+
+  def create
+    m = Message.new(message_params)
+    if m.save
+      @m = "The message has been sent successfully"
+    else
+      @m = "Something wrong hapeened, please try again"
+    end
+    render 'bootbox.js.erb'
+  end
+
+  private
+  def message_params
+    params.require(:messages).permit(:applicant_id, :content)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :sessions
+  get 'messages/new'
 
-  get 'top/index'
+  resources :sessions
+  resources :messages
+  resources :top
+
+  root 'top#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
